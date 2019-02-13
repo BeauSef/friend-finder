951aa2565275d75f143872d77c5efb0b66409aaa
Beau Hunter <hunter0907@gmail.com>
2019-02-12 23:16:51 -0700

    Added route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,10 @@ path = require("path");
 
 module.exports = function(app){
     app.get("/", function(req,res){
-        res.sendFile(path.join(_dirname, "../public/home.html"))
+        res.sendFile(path.join(__dirname, "../public/home.html"))
     });
 
-    app.get("/", function(req,res){
-        res.sendFile(path.join(_dirname, "../public/survey.html"))
+    app.get("/survey", function(req,res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"))
     });
 }
